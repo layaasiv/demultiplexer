7bf17041253f50e1050b06856fe7596fb7435f19
Layaa Sivakumar <layaasiv@LayaaLaptop.localdomain>
2026-08-27 20:13:20 -0400

    gives default input value for pytest as path to test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,18 +2,10 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--input", action="store", default=None)
-    parser.addoption("--output", action="store", default=None)
 
 @pytest.fixture
 def input_path(request):
     value = request.config.getoption("--input")
-    if value is None:
-        pytest.fail("--input is required for this test.")
-    return request.config.getoption("--input")
-
-# @pytest.fixture
-# def tmp_path(request):
-#     value = request.config.getoption("--output")
-#     if value is None:
-#         pytest.fail("--output is required for this test.")
-#     return value
+    if value is not None:
+        return Path(value)
+    return Path(__file__).parent / "data"
