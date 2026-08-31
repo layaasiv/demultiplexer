3da70c20027f7e909c629b5be605ba2f18794917
Layaa Sivakumar <layaasiv@LayaaLaptop.localdomain>
2026-08-30 23:16:36 -0400

    fix typo in test_write_record_to_file().

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -74,6 +74,6 @@ def test_write_record_to_file(tmp_path: str):
             qscores="quality scores"
         )
     
-    with gzip.open(output_file_path, "wt") as fh:
+    with gzip.open(output_file_path, "rt") as fh:
         assert sum(1 for _ in fh) > 0, "write_record_to_file output file is empty."
         assert sum(1 for _ in fh) == 4, "write_record_to_file output file does not have the expected number of lines."
